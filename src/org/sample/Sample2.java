30a2a5dbc2da6b2582332520f54ef4e042ae304c
unknown <katurijagadeesshabu27@gmail.com>
2021-01-20 22:47:50 +0530

    new updates

---
+++ src/org/sample/Sample2.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+import java.util.Scanner;
+
+public class Sample2 {
+public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Please enter any number");
+	int i=sc.nextInt();
+	int a,b;
+	for(a=1;a<=i;a++)
+	{
+		for(b=1;b<=a;b++) {
+			System.out.print("*" );
+		}
+		System.out.println("");
+		
+	}
+
+}
+}
